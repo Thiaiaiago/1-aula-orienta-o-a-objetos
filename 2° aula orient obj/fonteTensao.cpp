ee6d11046cf7374216bcf998c309f3a0f4731876
Thiaiaiago <109763990+Thiaiaiago@users.noreply.github.com>
2022-08-29 18:03:18 -0300

    Add files via upload

--- 2° aula orient obj/fonteTensao.cpp
+++ 2° aula orient obj/fonteTensao.cpp
@@ -1,11 +1,39 @@
 #include "fonteTensao.h"
 
-fonteTensao::fonteTensao()
+double fonteTensao::getv()//definindo que getv  é dessa class, pode ser que outrra classe tenha um metodo com mesmo nome
 {
-    //ctor
+
+    return vnominal;
 }
 
-fonteTensao::~fonteTensao()
+bool fonteTensao::setv(double v)
 {
-    //dtor
+    if (v<vmin||v>vmax)
+        return false;
+
+    vnominal=v;
+    return true;
 }
+
+//construtor sem parametros. funçao ou metodo para criar o objeto
+ fonteTensao::fonteTensao(void)
+{
+    vnominal=5;
+    vmin=0;
+    vmax=15;
+}
+
+//construtor com parametros. funçao ou metodo para criar o objeto com parametros expecificados
+ fonteTensao::fonteTensao(double valormin, double valormax)
+{
+    vmin=valormin;
+    vmax=valormax;
+    vnominal=5;
+}
+
+//destrutor.
+ fonteTensao::~fonteTensao(void)
+ {
+
+ }
+
